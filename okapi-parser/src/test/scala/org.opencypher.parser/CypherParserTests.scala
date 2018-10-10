78ca534bc241eef5f3b58fde565a5522e8f58cbd
Philip Stutz <philip.stutz@gmail.com>
2018-10-10 12:58:24 +0100

    WIP

--- okapi-parser/src/test/scala/org.opencypher.parser/CypherParserTests.scala
+++ okapi-parser/src/test/scala/org.opencypher.parser/CypherParserTests.scala
@@ -8,6 +8,10 @@ class CypherParserTests extends FunSpec with Matchers {
     CypherParser.parse("MATCH (a:A), (b:B)\nWITH [p = (a)-[*]->(b) | p] AS paths, count(a) AS c\nRETURN paths, c")
   }
 
+  it("with") {
+    CypherParser.withClause.parse("WITH [p = (a)-[*]->(b) | p] AS paths").get.value.show()
+  }
+
   it("individual parser") {
     CypherParser.patternComprehension.parse("[p = (a)-[*]->(b) | p]").get.value.show()
   }
